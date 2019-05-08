bb6840ded6d0d44fb295d61600fe0080710fd30b
Juliette Abeyta <jabeyta@fluentstream.com>
2019-05-08 09:26:20 -0600

    ➿ Add active state to naavlinks

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,17 +3,29 @@ import { Link } from "react-router-dom";
 import '../styles/navbar.scss';
 class NavBar extends Component {
 
+  handleClick(e){
+    let links = document.getElementsByClassName('route');
+    for (let i = 0; i < links.length; i++) {
+        let current = document.getElementsByClassName("active");
+        if (current.length > 0) {
+          current[0].className = current[0].className.replace(" active", "");
+        }
+    }
+    let link = e.currentTarget;
+    link.className += ' active';
+  }
+
   render() {
     return (
       <div className="nav-bar">
-        <Link to="/about" className="route">
-          <p className="nav-link">about</p>
+        <Link to="/about" className="route" onClick={(e) => this.handleClick(e)}>
+          <p className="nav-link" >about</p>
         </Link>
-        <Link to="/skills" className="route">
-          <p className="nav-link">skills</p>
+        <Link to="/skills" className="route" onClick={(e) => this.handleClick(e)}>
+          <p className="nav-link" >skills</p>
         </Link>
-        <Link to="/" className="route">
-          <p className="nav-link">home</p>
+        <Link to="/" className="route active" onClick={(e) => this.handleClick(e)}>
+          <p className="nav-link" >home</p>
         </Link>
       </div>
     );
