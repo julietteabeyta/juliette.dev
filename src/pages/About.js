4411c8fa2de1656249f359bd8de7bb16e7590ff9
Juliette Abeyta <jabeyta@fluentstream.com>
2019-05-09 20:44:33 -0600

    👋 Add about content

--- src/pages/About.js
+++ src/pages/About.js
@@ -10,7 +10,20 @@ class About extends Component {
 
     return (
       <div className="about page">
-          <ReactSVG className="juliette-svg"src={svgPath}/>
+        <div className="about-container">
+          <p>
+            Hey! It's me, Juliette; your average role-playing, coffee-drinking lover of all things horror.
+            I currently manage a full-stack development team focusing on writing stellar code and perpetuating
+            an inclusive, supportive culture. As a developer, I have a passion for building seamless
+            interactions and creating useful, beautiful digital experiences.
+          </p>
+          <br/>
+          <p>
+            In addition to development, I enjoy good books, riding fixed gear bikes around the city, and long walks on the beach.
+            And I am always looking to connect! You can connect with me on <a href="https://twitter.com/julietteabeyta" target="_blank" rel="noopener noreferrer">twitter</a> or shoot me an email <a href="mailto:julietteabeyta@gmail.com" target="_blank" rel="noopener noreferrer">here.</a>
+          </p>
+        </div>
+        <ReactSVG className="juliette-svg"src={svgPath}/>
       </div>
     );
   }
