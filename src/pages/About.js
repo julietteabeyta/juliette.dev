c864e58e00a5119f7983d49700f55bf9fb2c0599
Juliette Abeyta <jabeyta@fluentstream.com>
2019-05-11 11:58:00 -0600

    ❌ Remove unused variable deeclarations

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
-import ReactSVG from 'react-svg'
 import '../styles/about.scss';
 
 class About extends Component {
 
   render() {
-
-    const svgPath = 'juliette.svg';
 
     return (
       <div className="about page">
